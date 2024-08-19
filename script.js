e989cd5a9c579dc12a5a28cea32a0347e189812a
Srinu-7 <srinumaka9908@gmail.com>
2024-08-19 16:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+  if (!s) {
+    return '';
+  }
+
+  // Split the string by spaces and return the first element in the array
+  const words = s.trim().split(/\s+/);
+  return words[0];
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
